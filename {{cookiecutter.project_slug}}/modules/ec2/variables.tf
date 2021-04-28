0ac32a7050c12a6b3eb7dcac5d02ed3fba4edf20
Diego Alves <dalves@pagseguro.com>
2021-04-27 23:35:14 -0300

    variables comment

--- {{cookiecutter.project_slug}}/modules/ec2/variables.tf
+++ {{cookiecutter.project_slug}}/modules/ec2/variables.tf
@@ -1,12 +1,20 @@
+# -----------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# These variables must be set in the module block when using this module.
+# -----------------------------------------------------------------------------
 variable "image" {
   description = "Instance Image"
   type        = string
 }
 
+# -----------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These variables have default values and don't have to be set to use this module.
+# You may set these variables to override their default values.
+# -----------------------------------------------------------------------------
 variable "type" {
   description = "Instance Type"
   type        = string
   default     = "t2.micro"
   
 }
-
